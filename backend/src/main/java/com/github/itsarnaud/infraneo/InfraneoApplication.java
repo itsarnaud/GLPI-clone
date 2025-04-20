180531961efbabff94678ea9eece228618d11f75
Raphaël <115149179+rboucheron@users.noreply.github.com>
2025-04-20 12:57:42 +0200

    fix(infraneo1)fix-application-Class-name

--- backend/src/main/java/com/github/itsarnaud/infraneo/InfraneoApplication.java
+++ backend/src/main/java/com/github/itsarnaud/infraneo/InfraneoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IfraneoApplication {
+public class InfraneoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(IfraneoApplication.class, args);
+		SpringApplication.run(InfraneoApplication.class, args);
 	}
 
 }
